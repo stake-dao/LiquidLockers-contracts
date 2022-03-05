abfc9b175ba7045ba7ebd30b6c4fd1dd08cb986f
Andrea Zuccarini <andrearanma@gmail.com>
2022-03-05 17:28:26 +0100

    added deploy script for crv acc and depositor

---
+++ deploy/023_deploy_crvAccumulator.ts
@@ -0,0 +1,21 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments, getNamedAccounts } = hre;
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  const THREE_CRV = "0x6c3F90f043a72FA612cbac8115EE7e52BDe6E490";
+
+  await deploy("CURVEAccumulator", {
+    contract: "CurveAccumulator",
+    from: deployer,
+    args: [THREE_CRV],
+    log: true
+  });
+};
+export default func;
+
+func.skip = async () => false;
+func.tags = ["CurveAccumulator"];
---
+++ deploy/024_deploy_crvDepositor.ts
@@ -0,0 +1,24 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments, getNamedAccounts } = hre;
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  const CRV = "0xD533a949740bb3306d119CC777fa900bA034cd52";
+  const CRV_LOCKER = "0x52f541764E6e90eeBc5c21Ff570De0e2D63766B6";
+  const minter = await deployments.get("sdCRV");
+
+  await deploy("CRVDepositor", {
+    contract: "Depositor",
+    from: deployer,
+    args: [CRV, CRV_LOCKER, minter.address],
+    log: true
+  });
+};
+export default func;
+
+func.skip = async () => false;
+func.tags = ["CrvDepositor"];
+func.dependencies = ["sdCRV"];
