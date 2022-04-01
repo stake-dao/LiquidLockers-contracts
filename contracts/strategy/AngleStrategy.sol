cfefdf7946ceddf24ac70476b5a25b0498955c7d
iamsahu <praffulsahu@gmail.com>
2022-04-01 23:40:27 +0530

    feat: strategy proxy WIP

--- contracts/strategy/AngleStrategy.sol
+++ contracts/strategy/AngleStrategy.sol
@@ -1,14 +1,39 @@
 pragma solidity 0.8.7;
 
-import '../interfaces/ILiquidityGauge.sol';
-contract AngleStrategy {
-	function deposit(address to, uint256 amount) external {
-        ILiquidityGauge(to).deposit(amount);
-    }
+import "./BaseStrategy.sol";
 
-	function withdrawLPs(address to, uint256 amount) external {}
+contract AngleStrategy is BaseStrategy {
+	constructor(ILocker _locker, address _governance) BaseStrategy(_locker, _governance) {}
 
-	function claim(address to) external {}
+	function deposit(uint256 amount, address gauge) external override onlyGovernance {
+		locker.execute(gauge, 0, abi.encodeWithSignature("deposit(uint256)", amount));
+	}
+
+	function depositAll(address gauge) external override onlyGovernance {
+		locker.execute(
+			gauge,
+			0,
+			abi.encodeWithSignature(
+				"deposit(uint256)",
+				0 // Get the balance of
+			)
+		);
+	}
+
+	function withdraw(uint256 amount, address gauge) external override onlyGovernance {
+		locker.execute(gauge, 0, abi.encodeWithSignature("withdraw(uint256)", amount));
+	}
+
+	function withdrawAll(address gauge) external override onlyGovernance {
+		locker.execute(
+			gauge,
+			0,
+			abi.encodeWithSignature(
+				"withdraw(uint256)",
+				0 // Get the balance of
+			)
+		);
+	}
 
 	function sendToAccumulator(address to, uint256 amount) external {}
 }
--- contracts/strategy/BaseStrategy.sol
+++ contracts/strategy/BaseStrategy.sol
@@ -1,5 +1,24 @@
 pragma solidity 0.8.7;
+import "../interfaces/ILocker.sol";
 
 contract BaseStrategy {
-    
+	ILocker locker;
+	address governance;
+	modifier onlyGovernance() {
+		require(msg.sender == governance, "!governance");
+		_;
+	}
+
+	constructor(ILocker _locker, address _governance) public {
+		locker = _locker;
+		governance = _governance;
+	}
+
+	function deposit(uint256 amount, address gauge) external virtual onlyGovernance {}
+
+	function depositAll(address gauge) external virtual onlyGovernance {}
+
+	function withdraw(uint256 amount, address gauge) external virtual onlyGovernance {}
+
+	function withdrawAll(address gauge) external virtual onlyGovernance {}
 }
