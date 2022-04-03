68fa4ebfbe33577c78fb7986be4094226be7202a
Andrea Zuccarini <andrearanma@gmail.com>
2022-04-03 09:30:05 +0200

    added veSDT upgrade deploy script

---
+++ deploy/025_deploy_veSDT_upgrade.ts
@@ -0,0 +1,29 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+
+const SDT = "0x73968b9a57c6e53d41345fd57a6e6ae27d6cdb2f";
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments, getNamedAccounts } = hre;
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  await deploy("veSDTImplementationNew", {
+    contract: "veSDT",
+    from: deployer,
+    args: [],
+    log: true
+  });
+
+  const proxyAdmin = await deployments.get("ProxyAdmin");
+  const veSdtProxy = await deployments.get("veSDT");
+  const veSDTImplementationNew = await deployments.get("veSDTImplementationNew");
+
+  await proxyAdmin.upgrade(veSdtProxy.address, veSDTImplementationNew.address);
+};
+
+export default func;
+
+func.skip = async () => false;
+func.tags = ["veSDT"];
+func.dependencies = ["ProxyAdmin", "veSDT"];
