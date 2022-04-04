c981b439b661cba8f5c1821a6fd205696566b950
iamsahu <praffulsahu@gmail.com>
2022-04-04 12:01:55 +0530

    feat: angle strategy

--- contracts/strategy/AngleStrategy.sol
+++ contracts/strategy/AngleStrategy.sol
@@ -1,38 +1,60 @@
 pragma solidity 0.8.7;
-
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "./BaseStrategy.sol";
 
 contract AngleStrategy is BaseStrategy {
+	using SafeERC20 for IERC20;
+
+	/* ========== EVENTS ========== */
+	event Deposit(address _gauge, address _token, uint256 _amount);
+	event Withdraw(address _gauge, address _token, uint256 _amount);
+
+	/* ========== CONSTRUCTOR ========== */
 	constructor(ILocker _locker, address _governance) BaseStrategy(_locker, _governance) {}
 
-	function deposit(uint256 amount, address gauge) external override onlyGovernance {
-		locker.execute(gauge, 0, abi.encodeWithSignature("deposit(uint256)", amount));
+	/* ========== MUTATIVE FUNCTIONS ========== */
+	function deposit(
+		address _gauge,
+		address _token,
+		uint256 _amount
+	) public override onlyGovernance {
+		IERC20(_token).safeTransfer(address(locker), _amount);
+
+		locker.execute(_token, 0, abi.encodeWithSignature("approve(address,uint256)", _gauge, 0));
+		locker.execute(_token, 0, abi.encodeWithSignature("approve(address,uint256)", _gauge, _amount));
+
+		(bool success, ) = locker.execute(_gauge, 0, abi.encodeWithSignature("deposit(uint256)", _amount));
+		require(success, "Deposit failed!");
+		emit Deposit(_gauge, _token, _amount);
 	}
 
-	function depositAll(address gauge) external override onlyGovernance {
-		locker.execute(
-			gauge,
+	function depositAll(address _gauge, address _token) external override onlyGovernance {
+		deposit(_gauge, _token, IERC20(_token).balanceOf(address(this)));
+	}
+
+	function withdraw(
+		address _gauge,
+		address _token,
+		uint256 _amount
+	) public override onlyGovernance {
+		uint256 _before = IERC20(_token).balanceOf(address(locker));
+
+		locker.execute(_gauge, 0, abi.encodeWithSignature("withdraw(uint256)", _amount));
+		uint256 _after = IERC20(_token).balanceOf(address(locker));
+
+		uint256 _net = _after - _before;
+		(bool success, ) = locker.execute(
+			_token,
 			0,
-			abi.encodeWithSignature(
-				"deposit(uint256)",
-				0 // Get the balance of
-			)
+			abi.encodeWithSignature("transfer(address,uint256)", msg.sender, _net)
 		);
+		require(success, "Deposit failed!");
+		emit Withdraw(_gauge, _token, _amount);
 	}
 
-	function withdraw(uint256 amount, address gauge) external override onlyGovernance {
-		locker.execute(gauge, 0, abi.encodeWithSignature("withdraw(uint256)", amount));
-	}
-
-	function withdrawAll(address gauge) external override onlyGovernance {
-		locker.execute(
-			gauge,
-			0,
-			abi.encodeWithSignature(
-				"withdraw(uint256)",
-				0 // Get the balance of
-			)
-		);
+	function withdrawAll(address _gauge, address _token) external override onlyGovernance {
+		withdraw(_gauge, _token, IERC20(_token).balanceOf(_gauge));
 	}
 
 	function sendToAccumulator(address to, uint256 amount) external {}
--- contracts/strategy/BaseStrategy.sol
+++ contracts/strategy/BaseStrategy.sol
@@ -2,23 +2,42 @@ pragma solidity 0.8.7;
 import "../interfaces/ILocker.sol";
 
 contract BaseStrategy {
+	/* ========== STATE VARIABLES ========== */
 	ILocker locker;
 	address governance;
+
+	/* ========== MODIFIERS ========== */
 	modifier onlyGovernance() {
 		require(msg.sender == governance, "!governance");
 		_;
 	}
 
+	/* ========== CONSTRUCTOR ========== */
 	constructor(ILocker _locker, address _governance) public {
 		locker = _locker;
 		governance = _governance;
 	}
 
-	function deposit(uint256 amount, address gauge) external virtual onlyGovernance {}
+	/* ========== MUTATIVE FUNCTIONS ========== */
+	function deposit(
+		address _gauge,
+		address _token,
+		uint256 _amount
+	) external virtual onlyGovernance {}
 
-	function depositAll(address gauge) external virtual onlyGovernance {}
+	function depositAll(address _gauge, address _token) external virtual onlyGovernance {}
 
-	function withdraw(uint256 amount, address gauge) external virtual onlyGovernance {}
+	function withdraw(
+		address _gauge,
+		address _token,
+		uint256 _amount
+	) external virtual onlyGovernance {}
 
-	function withdrawAll(address gauge) external virtual onlyGovernance {}
+	function withdrawAll(address _gauge, address _token) external virtual onlyGovernance {}
+
+	function disableGauge(address _gauge) external virtual onlyGovernance {}
+
+	function boost(address _gauge) external {}
+
+	function set_rewards_receiver(address _receiver, address _gauge) external virtual onlyGovernance {}
 }
