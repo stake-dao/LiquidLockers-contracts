39dfc492ee1bd834dfa334e6f3c6255c4efd9dfa
Andrea Zuccarini <andrearanma@gmail.com>
2022-06-06 17:06:55 +0200

    last deploy scripts edits

--- deploy/044_deploy_BalZapper.ts
+++ deploy/044_deploy_BalZapper.ts
@@ -15,5 +15,5 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 };
 export default func;
 
-func.skip = async () => false;
+func.skip = async () => true;
 func.tags = ["BalancerZapper"];
