ef90e0a4adedb634a0c7904b59a4fb7066878a9c
Andrea Zuccarini <andrearanma@gmail.com>
2022-07-26 13:20:38 +0200

    claim all tokens in LFT LL test

--- test/step4_LendFlareLL.ts
+++ test/step4_LendFlareLL.ts
@@ -6,6 +6,7 @@ import { Contract } from "@ethersproject/contracts";
 import { BigNumber } from "@ethersproject/bignumber";
 import { JsonRpcSigner } from "@ethersproject/providers";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { writeBalance } from "./utils";
 
 import ERC20ABI from "./fixtures/ERC20.json";
 import VELFTABI from "./fixtures/veLFT.json";
@@ -19,6 +20,8 @@ import {
     VE_LFT,
     RANDOM,
     SDT,
+    DAI,
+    WBTC,
     STAKE_DAO_MULTISIG,
     VE_SDT,
     VE_SDT_BOOST_PROXY,
@@ -32,9 +35,12 @@ const ONE_YEAR_IN_SECONDS = 86_400 * 365;
 const ETH_100 = BigNumber.from(10).mul(BigNumber.from(10).pow(18)).toHexString();
 
 const USDC_HOLDER = "0x4f1a5a5d258522254933a6DA9648c57Fe230D17e";
+const WBTC_HOLDER = "0x3D2FB958376EE1cA22322B2c226666E414c3CFdd";
 const LENDFLARE_OWNER = "0x77Be80a3c5706973a925C468Bdc8eAcCD187D1Ba";
 const USDC_BASE_REWARD = "0xC5f11E8B5475FF47ba855ca459Ee817b343dD6E4";
 const ETH_BASE_REWARD = "0x37dd2f9b2FEF9Cc123C5a97CDDC76ea332B6E382";
+const DAI_BASE_REWARD = "0x59DFE3641993Eadbc42f16FC05b306DCEbCd7CE0";
+const WBTC_BASE_REWARD = "0x3cB2164E43c77A6De18ABcB955d1F1f66973C9C0";
 
 const getNow = async function () {
     let blockNum = await ethers.provider.getBlockNumber();
@@ -60,15 +66,20 @@ describe("LendFlare Depositor", function () {
     let lftToken: Contract;
     let feeDistributor: Contract;
     let usdc: Contract;
+    let dai: Contract;
     let weth: Contract;
+    let wbtc: Contract;
     let usdcBaseReward: Contract;
     let ethBaseReward: Contract;
+    let daiBaseReward: Contract;
+    let wbtcBaseReward: Contract;
 
     // Helper Signers
     let lftTokenHolder: JsonRpcSigner;
     let sdtDeployer: JsonRpcSigner;
     let lendFlareOwner: JsonRpcSigner;
     let usdcHolder: JsonRpcSigner;
+    let wbtcHolder: JsonRpcSigner;
 
     before(async function () {
         [deployer, alice, bob] = await ethers.getSigners();
@@ -76,11 +87,15 @@ describe("LendFlare Depositor", function () {
         // Tokens
         lftToken = await ethers.getContractAt(ERC20ABI, LFT);
         usdc = await ethers.getContractAt(ERC20ABI, USDC);
+        dai = await ethers.getContractAt(ERC20ABI, DAI);
         weth = await ethers.getContractAt(ERC20ABI, WETH);
+        wbtc = await ethers.getContractAt(ERC20ABI, WBTC);
 
         // base reward pool
         usdcBaseReward = await ethers.getContractAt(BASEREWARDABI, USDC_BASE_REWARD);
         ethBaseReward = await ethers.getContractAt(BASEREWARDABI, ETH_BASE_REWARD);
+        daiBaseReward = await ethers.getContractAt(BASEREWARDABI, DAI_BASE_REWARD);
+        wbtcBaseReward = await ethers.getContractAt(BASEREWARDABI, WBTC_BASE_REWARD);
 
         // veBAL
         veLFT = await ethers.getContractAt(VELFTABI, VE_LFT);
@@ -114,8 +129,16 @@ describe("LendFlare Depositor", function () {
         await network.provider.send("hardhat_setBalance", [USDC_HOLDER, ETH_100]);
         usdcHolder = ethers.provider.getSigner(USDC_HOLDER);
 
-        await usdc.connect(usdcHolder).transfer(lendFlareOwner._address, parseUnits("100", "6"))
+        await network.provider.request({
+            method: "hardhat_impersonateAccount",
+            params: [WBTC_HOLDER]
+        });
+        await network.provider.send("hardhat_setBalance", [WBTC_HOLDER, ETH_100]);
+        wbtcHolder = ethers.provider.getSigner(WBTC_HOLDER);
 
+        await usdc.connect(usdcHolder).transfer(lendFlareOwner._address, parseUnits("100", "6"));
+        await wbtc.connect(wbtcHolder).transfer(lendFlareOwner._address, parseUnits("1", "8"));
+        await writeBalance(DAI, "500", lendFlareOwner._address);
 
         // LendFlare Fee Distributor
         feeDistributor = await ethers.getContractAt(BalancerFeeDistributorABI, LENDFLARE_FEE_DISTRIBUTOR);
@@ -269,7 +292,7 @@ describe("LendFlare Depositor", function () {
             // Already locked to max / Need to just increase amount
             await lendFlareDepositor.setRelock(false);
             // Transfer to Locker
-            await lftToken.connect(lftTokenHolder).transfer(locker.address, amount)
+            await lftToken.connect(lftTokenHolder).transfer(locker.address, amount);
             // Lock through Depositor Function
             await lendFlareDepositor.lockToken();
 
@@ -322,19 +345,30 @@ describe("LendFlare Depositor", function () {
             // 4 - UNI V2 token
             // 5 - agEUR
             // simulate a notify amount
-            const usdcToNotify = parseUnits("100", "6")
-            const ethToNotify = parseEther("1")
-            await usdc.connect(lendFlareOwner).approve(usdcBaseReward.address, usdcToNotify)
+            const usdcToNotify = parseUnits("100", "6");
+            const wbtcToNotify = parseUnits("1", "8");
+            const daiToNotify = parseEther("100");
+            const ethToNotify = parseEther("1");
+            await usdc.connect(lendFlareOwner).approve(usdcBaseReward.address, usdcToNotify);
+            await dai.connect(lendFlareOwner).approve(daiBaseReward.address, daiToNotify);
             await usdcBaseReward.connect(lendFlareOwner).notifyRewardAmount(usdcToNotify);
             await ethBaseReward.connect(lendFlareOwner).notifyRewardAmount(ethToNotify);
+            await daiBaseReward.connect(lendFlareOwner).notifyRewardAmount(daiToNotify);
+            await wbtcBaseReward.connect(lendFlareOwner).notifyRewardAmount(wbtcToNotify);
             skip(86_400 * 7)
-            const usdcBefore = await usdc.balanceOf(accumulator.address)
-            const wethBefore = await weth.balanceOf(accumulator.address)
-            await locker.connect(sdtDeployer).claimRewards([0, 3], accumulator.address);
-            const usdcAfter = await usdc.balanceOf(accumulator.address)
-            const wethAfter = await weth.balanceOf(accumulator.address)
+            const usdcBefore = await usdc.balanceOf(accumulator.address);
+            const daiBefore = await dai.balanceOf(accumulator.address);
+            const wethBefore = await weth.balanceOf(accumulator.address);
+            const wbtcBefore = await wbtc.balanceOf(accumulator.address);
+            await locker.connect(sdtDeployer).claimRewards([0, 1, 2, 3], accumulator.address);
+            const usdcAfter = await usdc.balanceOf(accumulator.address);
+            const daiAfter = await dai.balanceOf(accumulator.address);
+            const wethAfter = await weth.balanceOf(accumulator.address);
+            const wbtcAfter = await wbtc.balanceOf(accumulator.address);
             expect(usdcAfter).gt(usdcBefore);
+            expect(daiAfter).gt(daiBefore);
             expect(wethAfter).gt(wethBefore);
+            expect(wbtcAfter).gt(wbtcBefore);
         });
     });
 });
