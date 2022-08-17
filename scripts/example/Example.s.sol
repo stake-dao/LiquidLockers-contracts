0cc1aa16d0ba312034bcf134c3ffce53ed52d2cc
Warren <99427649+kobe-eth@users.noreply.github.com>
2022-08-17 15:10:56 +0200

    feat: example deployment script

---
+++ scripts/example/Example.s.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity 0.8.7;
+
+import "forge-std/Test.sol";
+import "forge-std/Script.sol";
+
+import {ClaimRewards} from "contracts/staking/ClaimRewards.sol";
+
+contract ExampleScript is Script, Test {
+    ClaimRewards claimRewards;
+
+    function run() public {
+        vm.startBroadcast();
+
+        // That's it.
+        claimRewards = new ClaimRewards();
+        // Contract deployed.
+
+        vm.stopBroadcast();
+    }
+}
