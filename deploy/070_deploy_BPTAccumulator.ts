a1a74e0fc2b1fa61f7bd6d267cf34012d2c8007a
Warren <99427649+kobe-eth@users.noreply.github.com>
2022-08-22 18:03:39 +0200

    fix: tokenReward accumulator is WETH

--- deploy/070_deploy_BPTAccumulator.ts
+++ deploy/070_deploy_BPTAccumulator.ts
@@ -6,7 +6,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
 
-  const tokenReward = "0x0eC9F76202a7061eB9b3a7D6B59D36215A7e37da" // BPT
+  const tokenReward = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2" // WETH
   const gauge = "";
 
   await deploy("BlackpoolAccumulator", {
