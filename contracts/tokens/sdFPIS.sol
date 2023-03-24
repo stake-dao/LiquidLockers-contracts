c441c44cfd8b897c85c21fb1e08c3260c3107e09
PumpkingWok <andrearanma@gmail.com>
2023-03-24 11:02:20 +0100

    defined a minter/burner in the sdFPIS token

--- contracts/tokens/sdFPIS.sol
+++ contracts/tokens/sdFPIS.sol
@@ -8,36 +8,41 @@ import "openzeppelin-contracts/token/ERC20/ERC20.sol";
 /// @notice A token that represents the Token deposited by a user into the Depositor
 /// @dev Minting & Burning was modified to be used by the operators ennabled
 contract sdFPIS is ERC20 {
-    mapping(address => bool) public operators;
+    address public minter;
+    address public burner;
 
-    constructor() ERC20("Stake DAO FPIS", "sdFPIS") {}
-
-    /// @notice Enable a new operator that can mint and burn sdFPIS
-    /// @param _operator new operator address
-    function enableOperator(address _operator) external {
-        require(operators[msg.sender], "!authorized");
-        operators[_operator] = true;
+    constructor(address minter_, address burner_) ERC20("Stake DAO FPIS", "sdFPIS") {
+        minter = minter_;
+        burner = burner_;
     }
 
-    /// @notice Disable the caller operator, it can't disable another operator
-    function disableOperator() external {
-        require(operators[msg.sender], "!authorized");
-        operators[msg.sender] = false;
+    /// @notice Set a minter operator (only the actual minter can call it)
+    /// @param _minter new minter operator address
+    function setMinterOperator(address _minter) external {
+        require(msg.sender == minter, "!minter");
+        minter = _minter;
     }
 
-    /// @notice mint new sdFPIS, callable only by an operator enabled
+    /// @notice Set a burner operator (only the actual burner can call it) 
+    /// @param _burner new burner operator address
+    function setBurnerOperator(address _burner) external {
+        require(msg.sender == _burner, "!burner");
+        burner = _burner;
+    }
+
+    /// @notice mint new sdFPIS, callable only by the minter
     /// @param _to recipient to mint for
     /// @param _amount amount to mint
     function mint(address _to, uint256 _amount) external {
-        require(operators[msg.sender], "!authorized");
+        require(msg.sender == minter, "!minter");
         _mint(_to, _amount);
     }
 
-    /// @notice burn sdFPIS, callable only by the operator
+    /// @notice burn sdFPIS, callable only by the burner
     /// @param _from sdFPIS holder
     /// @param _amount amount to burn
     function burn(address _from, uint256 _amount) external {
-        require(operators[msg.sender], "!authorized");
+        require(msg.sender == burner, "!burner");
         _burn(_from, _amount);
     }
 }
